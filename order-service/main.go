61fcc04b522ea46c314714dd756164c124619f49
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-10-21 02:13:49 +0300

    Update main.go

--- order-service/main.go
+++ order-service/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"log"
 	"net"
+	"order-service/client"
 	"order-service/handler"
 	pb "order-service/proto"
-
 	"google.golang.org/grpc"
 )
 
 func main() {
+	// Создание gRPC клиента для Inventory Service
+	inventoryClient, err := client.NewInventoryClient("inventory-service:50056")
+	if err != nil {
+		log.Fatalf("Failed to connect to Inventory Service: %v", err)
+	}
+
 	lis, err := net.Listen("tcp", ":50054")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterOrderServiceServer(grpcServer, &handler.OrderServiceServer{})
+	pb.RegisterOrderServiceServer(grpcServer, handler.NewOrderServiceServer(inventoryClient))
 
 	log.Println("Order Service is running on port 50054")
 	if err := grpcServer.Serve(lis); err != nil {
